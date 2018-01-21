4bf269cace0ebd7ec64b324895dbbe6dce754a41
krzysiekb <kbr@tutanota.com>
2018-01-21 19:32:13 +0100

    marionette

--- tests/app_test.js
+++ tests/app_test.js
@@ -18,24 +18,4 @@ describe('Basic routes tests', function() {
         })
 
     })
-
-    it('GET to /pagecount should return 200', function(done){
-        chai.request(reqServer)
-        .get('/pagecount')
-        .end(function(err, res) {
-            res.should.have.status(200);
-            done();
-        })
-
-    })
-    
-    it('GET to /timeline should return 200', function(done){
-        chai.request(reqServer)
-        .get('/pagecount')
-        .end(function(err, res) {
-            res.should.have.status(200);
-            done();
-        })
-
-    })
 })
